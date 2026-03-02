b55059cdb651d8f78cb03cf10bb92ad5052adc89
Durgam44 <springlearning44@gmail.com>
2026-03-02 14:04:00 +0530

    updated App.jsx and added components folder

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <h1>this is a react appp to find out react working</h1>
+
+      <Home/>
     </>
   )
 }
